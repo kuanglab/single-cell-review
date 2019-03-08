7f06e97bb9f160ad44602b7f88e5909a614b3328
kuanglab <kuan0009@umn.edu>
2019-03-08 15:25:50 -0600

    Add files via upload

---
+++ PBMC/BackSPIN/script_backspin.m
@@ -0,0 +1,23 @@
+%% load data
+load A_samples y
+
+%% run backspin
+output = 'output.txt';
+tic
+system(['mypath/backspin -i data_backspin/A.cef -o ', output, ' -f 5000 -d 4 -v']);
+toc
+
+%% filter clustering results
+system(['head -n 8 ', output, ' > tmp.txt']);
+
+%% order cells according to original order
+res = readtable('tmp.txt','HeaderLines',2);
+delete('tmp.txt');
+cellnum = table2array(res(1,8:end));
+[~,idx] = sort(cellnum);
+
+c = table2array(res(end,8:end));
+c = c(idx);
+
+%% ARI
+disp(['ARI=',num2str(rand_index(c, y, 'adjusted'))]);
