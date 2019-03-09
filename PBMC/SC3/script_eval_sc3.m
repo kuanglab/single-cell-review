be450e6cb88f6700ebcbd2e4b25a0a63fecad924
kuanglab <kuan0009@umn.edu>
2019-03-08 21:51:37 -0600

    Update script_eval_sc3.m

--- PBMC/SC3/script_eval_sc3.m
+++ PBMC/SC3/script_eval_sc3.m
@@ -5,4 +5,4 @@ load ../A_samples
 r = readtable(['sc3_output.csv']);
 
 %% ARI
-disp(['ARI=',num2str(rand_index(r.sc3_10_clusters, y_10k, 'adjusted'))]);
+disp(['ARI=',num2str(rand_index(r.sc3_10_clusters, y_100, 'adjusted'))]);
