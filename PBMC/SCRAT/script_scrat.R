b92ead7ee732d5654304fa756e3f367533967605
kuanglab <kuan0009@umn.edu>
2019-03-08 23:06:47 -0600

    Create script_scrat.R

---
+++ PBMC/SCRAT/script_scrat.R
@@ -0,0 +1,19 @@
+library(scrat)
+
+data <- read.table("data_cidr/A_100.txt")
+
+start_time <- Sys.time()
+
+env <- scrat.new(list(dataset.name="Scrat 100 30 x 30 dmap", dim.1stLvlSom = 30, count.processing = TRUE, pseudotime.estimation=NULL, standard.spot.modules="dmap"))
+env$indata <- data
+
+set.seed(i)
+
+res <- scrat.run(env)
+
+end_time <- Sys.time()
+t = end_time - start_time
+print(t)
+
+name = "results_scrat/scrat_output.csv"
+write.csv(env$group.labels, name);
