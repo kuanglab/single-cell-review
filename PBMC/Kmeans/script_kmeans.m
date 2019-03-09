2f0e11c03fcccba05fc0c9b1255e3a4f63d4dbc1
kuanglab <kuan0009@umn.edu>
2019-03-08 21:27:28 -0600

    Update script_kmeans.m

--- PBMC/Kmeans/script_kmeans.m
+++ PBMC/Kmeans/script_kmeans.m
@@ -7,4 +7,4 @@ idx = kmeans(full(A_100), 10, 'display', 'iter', 'Replicates', 1);
 toc
 
 %% ari
-disp(rand_index(idx, y_100, 'adjusted'));
+disp(['ARI=',num2str(rand_index(idx, y_100, 'adjusted'))]);
